430e3cb9605956faf62b641b62d9c805840c1a92
Charles Roberts <cjroberts2013@gmail.com>
2022-07-03 16:03:04 -0400

    Initial Commit

---
+++ Christina Photo App/Christina_Photo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Christina_Photo_AppApp.swift
+//  Christina Photo App
+//
+//  Created by Charles Roberts on 7/3/22.
+//
+
+import SwiftUI
+
+@main
+struct Christina_Photo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
